76028af5f1210ee36b25ab4600a636934eea5072
Jordan Weeks <jdweeks2010@gmail.com>
2022-09-05 21:38:19 -0700

    finished, with ability to add other weeks

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,35 @@ useEffect(() =>{
   return (
     <main>
       <div className="balance-container">
-        <div>
-          <div>My balance</div>
-          <div>$921.48</div>
+        <div className='my-balance'>
+          <p>My balance</p>
+          <h2>$921.48</h2>
         </div>        
         <svg width="72" height="48" viewBox="0 0 72 48" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#382314" cx="48" cy="24" r="24"/><circle stroke="#FFF" strokeWidth="2" cx="24" cy="24" r="23"/></g></svg>
       </div>
       <div className="spending-container">
-        <div>Spending last 7 days</div>
-        {week.map((day) => (
-          <Bar key={day.day} day={day}/>) )}
+        <h2>Spending - last 7 days</h2>
+        <div className="chart">
+          {week.map((day) => (
+            <div className="day">
+              <Bar key={day.day} day={day} week={week}/>
+            </div>
+          ))}
+        </div>
+        <div className="month-container">
+          <div className="total-container">
+            <p>Total this month</p>
+            <h2>$478.33</h2>
+          </div>
+          
+          <div className="percent-container">
+            <div>+2.4%</div>
+            <p>from last month</p>
+          </div>
+        </div>
+        
+        
+        
 
       </div>
       
--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import '../styles/bar.css'
 
-const Bar = ({day}) => {
+const Bar = ({day, week}) => {
+  const [hovering, setHovering] = useState(false)
+  const largest = useRef(0)
+  const handleMouseOver = () =>{
+    setHovering(true)
+  }
+  const handleMouseOut = ()=>{
+    setHovering(false)
+  }
+  useEffect(() => {
+    const findLargest = () =>{
+      const highestSpendingDay = week.reduce((prevLargest, currentDay) =>{
+        return (currentDay.amount > prevLargest) ? currentDay.amount : prevLargest
+        // return highestSpendingDay
+      }, 0)
+      largest.current=highestSpendingDay
+    }
+    findLargest()
+  }, [])
+  
+  
+  // findLargest()
   return (
-    <>
+    <div className="day-container" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <div className='day-spent'>${day.amount}</div>      
+      <div className='bar-display'
+        style={{
+          height:4*day.amount,
+          backgroundColor: day.amount === largest.current ? 'var(--cyan)' : 'var(--soft-red)'
+        }}></div>
     <div>{day.day}</div>
-    <div>${day.amount}</div>
-    </>
-    
-
-  )
+    </div>
+    )
 }
 
 export default Bar
